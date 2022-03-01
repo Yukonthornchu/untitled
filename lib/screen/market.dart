86d885b0768c6a08cca89bc167bbea3b59bf8ee9
kong1510 <pakakida_kong@hotmail.com>
2022-03-01 16:31:17 +0700

    BitFriend ล่าสุด 3/1/2022 test api success

--- lib/screen/market.dart
+++ lib/screen/market.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert'as convert;
+import 'package:http/http.dart' as http;
+
+
 
 class market extends StatefulWidget {
   const market({Key? key}) : super(key: key);
@@ -12,18 +14,36 @@ class market extends StatefulWidget {
 class _marketState extends State<market> {
   List _Data = [];
   var _newCase = 0;
+  var _total = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    getData().then((value) =>
+        print(value)
+    );
+    getData();
+  }
 
   Future<String> getData() async{
 
+    // var url = Uri.http('https//covid19.ddc.moph.go.th','/api/Cases/today-cases-all');
+    // var response = await http.get("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all");
     var url =
-      Uri.http('covid19.ddc.moph.go.th','/api/Cases/today-cases-all');
+    Uri.https('covid19.ddc.moph.go.th','/api/Cases/today-cases-all');
+
+    // Await the http get response, then decode the json-formatted response.
     var response = await http.get(url);
     if (response.statusCode == 200) {
       var jsonResponse = convert.jsonDecode(response.body);
       // print(jsonResponse);
       this.setState(() {
-        _Data = jsonResponse;
+        // _Data = jsonResponse;
         _newCase = jsonResponse[0]['new_case'];
+        _total = jsonResponse[0]['total_case'];
+        print(jsonResponse);
+        print("----------------");
+        print(jsonResponse[0]['new_case']);
 
       });
     }
@@ -31,23 +51,23 @@ class _marketState extends State<market> {
     return 'done';
   }
 
-  @override
-  void initState() {
 
-    getData();
-  }
   Widget build(BuildContext context) =>  Scaffold(
         // appBar: AppBar(
         //   title: Text('Market'),
         //   backgroundColor: Colors.black,
-        // ),
+        // )
+
         body: Column(
           children: [
             Text('Market ${_newCase}', style: TextStyle(fontSize: 40)),
+            Text('Market ${_total}', style: TextStyle(fontSize: 40)),
+
             // Text('Market ${_Data[0]['total_case'] ?? ''}', style: TextStyle(fontSize: 60)),
 
 
           ],
+
         ),
 
 
--- lib/screen/notification.dart
+++ lib/screen/notification.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert'as convert;
+import 'package:http/http.dart' as http;
 
 class notification extends StatefulWidget {
   const notification({Key? key}) : super(key: key);
@@ -8,7 +10,26 @@ class notification extends StatefulWidget {
 }
 
 class _notificationState extends State<notification> {
-  @override
+  // @override
+  // void initState(){
+  //   super.initState();
+  //   getData().then((value) =>
+  //   print(value)
+  //   );
+  // }
+  //
+  // Future<String> getData() async{
+  //   var response = await http.get("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all");
+  //   if(response.statusCode == 200) {
+  //     var jsonResponse = convert.jsonDecode(response.body);
+  //     this.setState(() {
+  //       print(jsonResponse);
+  //       print('---------');
+  //       print(jsonResponse[0]['new_case']);
+  //     });
+  //   }
+  //   return 'done';
+  // }
   Widget build(BuildContext context) {
     return Scaffold(
       // appBar: AppBar(
